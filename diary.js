de98a0ca5b6024ee6c0486e6546260da67523d9c
khavinshankar <khavinshankar@gmail.com>
2021-01-02 00:41:32 +0530

    log command added

--- diary.js
+++ diary.js
@@ -19,7 +19,7 @@ switch (operation) {
     reply = add(...args);
     break;
   case "log":
-    log(...args);
+    reply = log(...args);
     break;
   case "cng":
     cng(...args);
@@ -28,8 +28,10 @@ switch (operation) {
     del(...args);
     break;
   default:
-    console.log("ERROR: unidentified operation, try `diary help` for more info");
+    console.log('ERROR: unidentified operation, try `diary help` for more info');
 }
+
+console.log(reply);
 
 function help() {
   const info = `Usage :-
@@ -39,36 +41,51 @@ function help() {
   
   Commands:
     $ diary add "entry content" [date] [time]    # Add an entry 
-    $ diary log date [index]                     # Log a specific entry or all entries on a date
-    $ diary del date [index]                     # Delete a specific entry or all entries on a date
+    $ diary log [date] [index]                   # Log a specific entry or all entries on a date
+    $ diary del [date] [index]                   # Delete a specific entry or all entries on a date
     $ diary cng date index "new content"         # Edit an entry
     $ diary help                                 # Show usage
   
   Default Values:
-    [date]  - today
-    [time]  - now
-    [index] - 1
+    [date]  - today // current day's date
+    [time]  - now   // current time
+    [index] - 0     // 0 means all
   `;
   console.log(info);
 }
 
 function add(content, date=today, time=now) {
-  if(content === undefined) return "ERROR: content not availabe";
-
+  if(content === undefined) return `ERROR: content not availabe`;
+  
   const [day, month, year] = date.split("-");
   const y_dir = path.join(__dirname, `${year}`);
   const m_dir = path.join(y_dir, `${month}`);
   if (!fs.existsSync(y_dir)) fs.mkdirSync(y_dir);
   if (!fs.existsSync(m_dir)) fs.mkdirSync(m_dir);
 
-  const file = path.join(m_dir, `${date}.txt`);
+  const file = path.join(m_dir, `${day}.txt`);
   const message = `[${time}] ${content}\n`;
   fs.appendFileSync(file, message);
 
-  return "Entry added successfully :)";
+  return `Entry added successfully :)`;
 }
 
-// diary add message date time
+function log(date=today, index=0) {
+  const [day, month, year] = date.split("-");
+  const y_dir = path.join(__dirname, `${year}`);
+  const m_dir = path.join(y_dir, `${month}`);
+  const file = path.join(m_dir, `${day}.txt`);
+  if (fs.existsSync(file)) {
+    const entries = fs.readFileSync(file, "utf-8").trim().split("\n");
+    if(index > entries.length) return `Error: index out of bounds`;
+
+    let result = "";
+    if(index == 0) for(let i=0; i<entries.length; i++) result += `<${i+1}> ${entries[i]}\n`;
+    else result = entries[index-1];
+
+    return result;
+  } else return `Sorry, no entries found on ${date} :(`;
+}
+
 // diary del date [index]
 // diary cng date index "new content"
-// diary log date [index]
